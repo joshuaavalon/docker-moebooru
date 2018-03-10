ec5f696e50359b9a492a83c2d4584b2a3e24f1e6
Joshua Avalon <joshuaavalon@gmail.com>
2018-03-10 13:25:01 +0800

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:2-slim
+FROM ruby:2
 
 ENV RAILS_ENVIRONMENT=production
 
--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@ if [ ! -d /data/app ]; then
     echo "Cannot find server folder in /data. Initialize moebooru."
     mv /moebooru/* /data/
     mv /moebooru/.git /data
+    cd /data
     bundle exec rake db:reset
     bundle exec rake db:migrate
 fi
+
+cd /data
 bundle exec unicorn
